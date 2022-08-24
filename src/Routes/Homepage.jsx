e7ffa6ae27bb91102822462ef15867d91dc94d4e
Priyanshu-Tyagi <102712899+Priyanshu-Tyagi@users.noreply.github.com>
2022-08-24 17:24:15 +0530

    Footer

--- src/Routes/Homepage.jsx
+++ src/Routes/Homepage.jsx
@@ -1,10 +1,12 @@
 import Navbar from "../Components/common/Navbar/Navbar";
+import Footer from "../Components/common/Footer/Footer";
 
 export default function Homepage(){
     return (
         <div>
             <Navbar />
             HomePage
+            <Footer/>
         </div>
     )
 }
