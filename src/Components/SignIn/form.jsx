3a886ec14d8c124def63677266274be2691da312
Priyanshu-Tyagi <102712899+Priyanshu-Tyagi@users.noreply.github.com>
2022-08-27 22:43:31 +0530

    compeleted Login

---
+++ src/Components/SignIn/form.jsx
@@ -0,0 +1,42 @@
+import {
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  InputGroup,
+  InputRightElement,
+  Text,
+} from "@chakra-ui/react";
+import { useState } from "react";
+
+export default function Form() {
+  const [show, setShow] = useState(false);
+  const handleClick = () => setShow(!show);
+
+  return (
+    <FormControl>
+      <FormLabel className="sign-input-lable">Email address</FormLabel>
+      <Input padding={'1.5rem'} type="email" />
+
+      <br />
+
+      {/* PASSWORD */}
+
+      <FormLabel className="sign-input-lable">Password</FormLabel>
+      <InputGroup size="md">
+        <Input padding={'1.5rem'} pr="4.5rem" type={show ? "text" : "password"} />
+        <InputRightElement width="4.5rem">
+          <Text
+            _hover={{ cursor: "pointer" }}
+            color={"#00819d"}
+            textDecoration={"underline"}
+            fontSize="15px"
+            onClick={handleClick}
+          >
+            {show ? "Hide" : "Show"}
+          </Text>
+        </InputRightElement>
+      </InputGroup>
+    </FormControl>
+  );
+}
---
+++ src/Components/common/Navbar/NavbarLite.jsx
@@ -0,0 +1,37 @@
+import {
+    Container,
+    Image,
+  } from "@chakra-ui/react";
+  import "../../../styles/navbar.css";
+  import { NavLink } from "react-router-dom";
+
+export default function Navbar2() {
+  return (
+    <div>
+      <Container
+        display="flex"
+        justifyContent="space-between"
+        maxW="95%"
+        borderBottom="2px solid #acb4b5"
+        padding="1rem 0rem"
+      >
+        <NavLink to="/">
+          <div>
+            <Image
+              src="https://drive.google.com/uc?export=view&id=1-b3mr0kQ2fDWlUQynCSSHzihlV0aHUuy"
+              alt="Logo"
+            />
+            {/* https://drive.google.com/file/d/1-b3mr0kQ2fDWlUQynCSSHzihlV0aHUuy/view?usp=sharing */}
+          </div>
+        </NavLink>
+        <NavLink to="/bag">
+          <img
+            src="https://drive.google.com/uc?export=view&id=15IbQEr6POwfJwaRI2f98m2YcTlCaz9FX"
+            alt="15IbQEr6POwfJwaRI2f98m2YcTlCaz9FX"
+          />
+          {/* https://drive.google.com/file/d/15IbQEr6POwfJwaRI2f98m2YcTlCaz9FX/view?usp=sharing */}
+        </NavLink>
+      </Container>
+    </div>
+  );
+}
